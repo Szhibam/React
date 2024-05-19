5f21fcd3420e8da519f4f8343c237e041d3ffbb7
Szhibam <shibambanerjeezxofficial@gmail.com>
2024-05-19 10:57:38 +0530

    1st React based code base using vite

---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,17 @@
+import Chai from "./chai"
+
+function App() {
+  
+
+  return (
+    <>
+    <h2>1. function ka naam caps me</h2>
+    <h3>2. return tum ek hi elements kr skte ho</h3>
+    <h3>isiliye usko iske andar kro</h3>
+    <h3> isko fragments bolte h</h3>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ 01viteReact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h2>Shibam chai pe js header bana rha h</h2>
+    )
+}
+
+export default Chai
