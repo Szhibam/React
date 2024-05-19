c18636284e0751bcc164755802a09c41e05440dc
Szhibam <shibambanerjeezxofficial@gmail.com>
2024-05-19 21:57:23 +0530

    App.jsx bas ek function h to use main.jsx k andar likh k dekhe?

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,6 +1,10 @@
 import Chai from "./chai"
 
 function App() {
+
+
+  // agar aapko veriable return krna h to kaise kroge?
+  const myVariable = "{} braces k andar variable ka naam likhese wo dikhaga"
   
 
   return (
@@ -9,6 +13,8 @@ function App() {
     <h3>2. return tum ek hi elements kr skte ho</h3>
     <h3>isiliye usko iske andar kro</h3>
     <h3> isko fragments bolte h</h3>
+    <p>note-- {myVariable}</p>  
+    {/* ____EVALUATED EXPRESSION^^^__________ */}
     <Chai/>
     </>
   )
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,9 +2,73 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// function is just an app so i can create a function here also
+// lets do that 
+
+function MyApp(){
+    return (
+    <div>
+        <h1>Custom app!!!</h1>
+    </div>)
+}
+
+/** ----------------------------------------------
+const reactElement = {
+    type : 'a'  ,   //'div/p/a'
+    props: {        //object hota h
+        href : 'https://google.com',
+        target : '_blank'
+    },
+    Children: 'Click me to visit google'
+}
+*/
+// ---------------------------------------------------------
+/*
+ * <MyApp> ('m' capital) ko vej rhe h jo fir react ek tree like structure me purse krta h
+ * iske liye binders use hote h
+ * to kyu na hum khud usko ek tree like structure de 
+ * lekin aisa krne se kya wo run hoga ?
+ * agar render k andar hum <ReactElement> pass krte h to kya wo run hoga ? or hume anchor tag dikhega?
+ * nhi!!!!! render ek function h jo ek perticular argument expect krta h aise akise run krega be
+ */
+
+const anotherElement = (
+    <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+
+// react k hisab se ek element banate h
+
+/**
+ * agar render k andar variable inject krna h to 
+ * 1. pehle usko render k bahar hi declare karo
+ * 2. fir usko rendar k andar ekkkduuum last me likh do
+ */
+
+let myVar = "ami ekta variable"
+
+const reactelement = React.createElement(
+    'a',  // the first parameter expected is the tag
+    {            // 2nd parameter expected is an object
+        href : "https://google.com",
+        target : '_blanl'
+    },
+    'click me visit googleeeeee ',
+    myVar
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <App />
+    // <MyApp/>
+    // <ReactElement/> -----> XXXX wont run
+    // reactelement    -----> XXXXX wont run
+    // anotherElement    // -----> ye visible hai
+
+    reactelement
   
 )
+
+// react ka render function :: ek perticular syntax h
